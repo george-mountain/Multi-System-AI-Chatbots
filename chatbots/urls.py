4262bb39c1e4a70cc25ea1f8d27330ffeeb5ab33
george-mountain <georgejiokem@gmail.com>
2023-06-18 14:53:17 +0900

    Implemented the clear chats archive functionality

--- chatbots/urls.py
+++ chatbots/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.chats, name="chats"),
+    path("chats/clear/", views.clear_chats, name="clear_chats"),
 ]
--- chatbots/views.py
+++ chatbots/views.py
@@ -1,13 +1,19 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth import get_user_model
 from django.http import JsonResponse
 from django.utils import timezone
 from .models import Chat
 from .utils import chat_openai_helper_function
 
+from django.contrib.auth.decorators import login_required
 
+
+@login_required
 def chats(request):
     chats = Chat.objects.filter(user=request.user)
+    recentChats = Chat.objects.filter(user=request.user).order_by("-created_at")[
+        :10
+    ]  # Fetch the last 10 chat conversations
 
     if request.method == "POST":
         message = request.POST.get("message")
@@ -22,5 +28,18 @@ def chats(request):
         chat.save()
         return JsonResponse({"message": message, "response": response})
 
-    context = {"chats": chats}
+    recent_chats = [
+        {"message": chat.message, "response": chat.response} for chat in recentChats
+    ]
+
+    context = {
+        "recent_chats": recent_chats,
+        "chats": chats,
+    }
     return render(request, "chats/chat_page.html", context)
+
+
+@login_required
+def clear_chats(request):
+    Chat.objects.filter(user=request.user).delete()
+    return redirect("chats")  # Redirect back to the chat page after clearing the chats
